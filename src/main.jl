9a0d7ffd69a4d21813be32e7c7c04cfac89cfecf
Elston Tan <elston1703@gmail.com>
2022-11-17 15:26:25 +0800

    Update main.jl

--- src/main.jl
+++ src/main.jl
@@ -1,4 +1,6 @@
+include("./funcs.jl")
 using PlotlyJS, Random
+
 
 points = 100
 epochs = 50
@@ -9,21 +11,8 @@ c = 0
 xs = randn(100) .+ 5
 ys = randn(100) .+ 5
 
-for i in 1:epochs 
-    global m
-    global c
-    y_pred = m .*  xs .+ c 
-    D_m = (-2/points) * sum(xs .* (ys - y_pred))
-    D_c = (-2/points) * sum(ys - y_pred)
-    m = m - lr*D_m 
-    c = c - lr*D_c 
-
-end
-eqn(x) = m*x+c 
-xline = [1:1:points;]
-yline = eqn.(xline)
-
-plot([
-    scatter(x=xs, y=ys, mode="markers", name="points"),
-    scatter(x=xline, y=yline, mode="lines", name="bestfitLine")
-])
+m, c = train(xs, ys, epoch, lr, m, c)
+visualize(xs, ys, m, c)
+f(x) = m*x+c
+value = rand(range(1, 20))
+pred = f(value)
